cffb606038542c77fc9b077284b7a2de3009055a
JP Meijers <jp.meijers@canonical.com>
2026-01-27 16:57:40 +0200

    Add npu test

--- agent-scripts/setup-dut.sh
+++ agent-scripts/setup-dut.sh
@@ -24,21 +24,3 @@ echo "::group::Installing machine dependencies"
 _run sudo apt-get install --yes git
 _run sudo snap install go --classic --no-wait
 echo "::endgroup::"
-
-if [[ -n "${INSTALL_NVIDIA_DRIVER_VERSION}" ]]; then
-  echo "::group::Installing NVIDIA driver $INSTALL_NVIDIA_DRIVER_VERSION"
-  _run sudo apt-get update
-  _run sudo apt-get install -y nvidia-driver-$INSTALL_NVIDIA_DRIVER_VERSION
-
-  # Reboot the device to load NVIDIA drivers
-  # In background to avoid breaking the SSH connection prematurely
-  echo "Rebooting the device"
-  ssh ubuntu@$DEVICE_IP "(sleep 3 && sudo reboot) &"
-
-  # Wait for shutdown to happen
-  sleep 10
-
-  # Wait for reboot
-  wait_for_ssh --allow-degraded || exit 1
-  echo "::endgroup::"
-fi
