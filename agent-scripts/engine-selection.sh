4cb56faa75af0b91941821569f2941cf9f9f3acc
JP Meijers <jp.meijers@canonical.com>
2026-02-02 14:14:58 +0200

    Retry engine selection

--- agent-scripts/engine-selection.sh
+++ agent-scripts/engine-selection.sh
@@ -3,15 +3,24 @@
 # Force select an engine if variable is set
 if [[ -n "${SELECT_ENGINE}" ]]; then
   echo "::group::Manually selecting engine"
-  # Engine might install two large components
-  # If the first one times out, try again to trigger the second one.
-  _run sudo "$SNAP_NAME" use-engine "$SELECT_ENGINE" || true
-  wait_for_snap_changes
-  _run sudo "$SNAP_NAME" use-engine "$SELECT_ENGINE" || true
-  wait_for_snap_changes
-  _run sudo "$SNAP_NAME" use-engine "$SELECT_ENGINE"
-  wait_for_snap_changes
-
+  # Engine might install multiple large components.
+  # Retry periodically to install missing components.
+  max_retries=30
+  retry_count=0
+  retry_delay=60
+  until _run sudo "$SNAP_NAME" use-engine "$SELECT_ENGINE"; do
+    retry_count=$((retry_count + 1))
+    if [ $retry_count -ge $max_retries ]; then
+      echo "Get logs"
+      _run sudo journalctl -a | grep "$SNAP_NAME"
+      echo "::error::Machine: $dut_hostname, failed to use-engine after trying $((max_retries * 30)) seconds"
+      exit 1
+    fi
+    echo "✘ $SNAP_NAME use-engine failed, retrying in ${retry_delay}s... ($retry_count/$max_retries)"
+    sleep $retry_delay
+  done
+  echo "✔ Selecting engine succeeded"
+  
   # Restart server after changing engine
   _run sudo snap restart "$SNAP_NAME"
   wait_for_snap_changes
