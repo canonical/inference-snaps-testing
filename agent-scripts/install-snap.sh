cccbcd99ae4d4b34f92f21e908f874b52902e191
Antonio Marangi <antonio.marangi@canonical.com>
2026-03-09 16:31:51 +0100

    Add tests for gemma3 on AMD gpu (#10)

--- agent-scripts/install-snap.sh
+++ agent-scripts/install-snap.sh
@@ -37,3 +37,21 @@ until _run $SNAP_NAME status; do
 done
 echo "✔ Snap status succeeded"
 echo "::endgroup::"
+if [ -n "$SNAP_CONNECTIONS" ]; then
+  echo "::group::Snap connections"
+  # Temporarily set IFS to a comma just for the 'read' command  
+  # -r prevents backslash escaping  
+  # -a assigns the result to an array named 'my_array'  
+  IFS=',' read -r -a my_array <<< "$SNAP_CONNECTIONS"  
+
+  # Iterate over the new array safely  
+  for connection in "${my_array[@]}"; do  
+      echo "Processing: $connection"  
+      _run sudo snap connect $SNAP_NAME:$connection  
+  done
+  wait_for_snap_changes
+  echo "::endgroup::"
+fi
+echo "::group::Checking snap connections"
+_run sudo snap connections "$SNAP_NAME"
+echo "::endgroup::"
