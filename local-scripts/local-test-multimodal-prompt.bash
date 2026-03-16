a16d78e413c00f91d92e437ff8da9f3fbf8db9c6
Antonio Marangi <antonio.marangi@canonical.com>
2026-03-16 11:24:50 +0100

    remove sourcing bash scripts when not necessary

--- local-scripts/local-test-multimodal-prompt.bash
+++ local-scripts/local-test-multimodal-prompt.bash
@@ -4,4 +4,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Setting configuration variables"
 source "${SCRIPT_DIR}/env.bash"
 
-source "${SCRIPT_DIR}/../agent-scripts/test-vlm.sh"
+${SCRIPT_DIR}/../agent-scripts/test-vlm.sh
